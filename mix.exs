67a5633c83825c793104c25e01a7206aa9f24a0b
surrsurus <surrsurus@protonmail.com>
2024-03-28 15:24:49 -0400

    Push package info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule OpenApiSpexTypedStruct.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "Open API Spex Typed Struct",
+      source_url: "https://github.com/surrsurus/open_api_spex_typed_struct",
+      description: "Automatically generate api specs for your typed structs"
     ]
   end
 
@@ -24,6 +28,15 @@ defmodule OpenApiSpexTypedStruct.MixProject do
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:open_api_spex, "~> 3.16"},
       {:typed_struct, "~> 0.3.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      name: "open_api_spex_typed_struct",
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["BSD-2-Clause"],
+      links: %{"GitHub" => "https://github.com/surrsurus/open_api_spex_typed_struct"}
     ]
   end
 end
